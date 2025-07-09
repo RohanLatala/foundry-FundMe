471fe2e6a89d156f456badda55ac8b86d4b82bf6
Rohanlatala <rohanlatala4@gmail.com>
2025-07-09 16:21:14 +0530

    fix: increased ETH value in integration test

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -7,7 +7,7 @@ import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
 import {FundMe} from "../src/FundMe.sol";
 
 contract FundFundMe is Script {
-    uint256 constant SEND_VALUE = 0.01 ether;
+    uint256 constant SEND_VALUE = 0.1 ether;
 
     function fundFundMe(address mostRecentlyDeployed) public {
         FundMe(payable(mostRecentlyDeployed)).fund{value: SEND_VALUE}();
@@ -16,7 +16,10 @@ contract FundFundMe is Script {
     }
 
     function run() external {
-        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("FundMe", block.chainid);
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "FundMe",
+            block.chainid
+        );
         vm.startBroadcast();
         fundFundMe(mostRecentlyDeployed);
         vm.stopBroadcast();
@@ -31,7 +34,10 @@ contract WithdrawFundMe is Script {
     }
 
     function run() external {
-        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("FundMe", block.chainid);
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "FundMe",
+            block.chainid
+        );
         vm.startBroadcast();
         withdrawFundMe(mostRecentlyDeployed);
         vm.stopBroadcast();
